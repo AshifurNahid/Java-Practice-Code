ea8a1e7fae86a77c10c6b9fd840117e2a9e7c53f
Ashifur Rahman <97047681+AshifurNahid@users.noreply.github.com>
2024-11-06 11:30:50 +0600

    First Commit

---
+++ Java-PracticeHub/src/com/javaproject/streams/StreamsApi.java
@@ -0,0 +1,34 @@
+package com.javaproject.streams;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamsApi {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList( 5,3,4,1,10,4,1);
+        System.out.println("Before filtering");
+
+        List<Integer> list = numbers.stream()
+                .filter(n ->{
+                    System.out.println("Filtering " + n);
+                    return n > 2;
+                }
+                )
+                .map(n -> {
+                    System.out.println("Mapping " + n);
+                    return n*2;
+                }).sorted().toList();
+
+        System.out.println("After filtering"+list);
+
+
+
+    }
+
+
+}
